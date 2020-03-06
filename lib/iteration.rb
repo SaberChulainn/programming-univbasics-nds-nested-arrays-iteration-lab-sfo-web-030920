0774acc60ff36db2b4e54917bb320cc8223b8a43
lively-bundle-0725 <tbandachit@gmail.com>
2020-03-06 07:25:26 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,11 +7,11 @@ def join_ingredients(src)
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
   row_index = 0
-  
+  string = []
   while row_index < src.count do 
       element_index = 0 
       while element_index < src[row_index].count do
-        return "I love " + src[row_index][element_index] + " and " + src[row_index][element_index + 1] + " on my pizza"
+        string.push("I love " + src[row_index][element_index] + " and " + src[row_index][element_index + 1] + " on my pizza")
         element_index += 1
       end
       row_index += 1
